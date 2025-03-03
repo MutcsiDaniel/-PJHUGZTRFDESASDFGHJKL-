8b7ad48d5149916a9d3bfc5cfbf277a4ada7ecb0
MutcsiDaniel <mucs.daniel-2022@keri.mako.hu>
2025-03-03 10:40:13 +0100

    ztf

--- JS/menu.js
+++ JS/menu.js
@@ -1,8 +1,20 @@
-function menu() {
-    var x = document.getElementById("osszecsukhato");
-    if (x.className === "fejlec") {
-      x.className += " responsive";
-    } else {
-      x.className = "fejlec";
-    }
-  } 
+function toggleSzoveg() {
+  var szovegElem = document.getElementById("dropdown1");
+
+  if (szovegElem.style.maxHeight === "0px" || szovegElem.style.maxHeight === "") {
+      szovegElem.style.maxHeight = szovegElem.scrollHeight + "px";
+      szovegElem.style.padding = "10px";
+  } else {
+      szovegElem.style.maxHeight = "0px";
+      szovegElem.style.padding = "0 10px";
+  }
+}
+
+function toggleSzoveg2() {
+  var szovegElem = document.getElementById("dropdown2");
+  if (szovegElem.style.display === "none") {
+      szovegElem.style.display = "block";
+  } else {
+      szovegElem.style.display = "none";
+  }
+}
