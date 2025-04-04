513fd6e95d6c7db20e712538b72c00d7c4fec2cc
MutcsiDaniel <mucs.daniel-2022@keri.mako.hu>
2025-04-04 09:12:30 +0200

    w4sertfgzbuhj

--- JS/idopont.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("ok").addEventListener("change", function() {
-    let selectedOption = this.options[this.selectedIndex];
-    let color = selectedOption.getAttribute("data-color");
-    this.style.backgroundColor = color;
-});
