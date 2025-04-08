d6da90a7a1b7fe5afb3d6f611e18055253d0ece7
MutcsiDaniel <mucsi.daniel-2022@keri.mako.hu>
2025-04-08 17:09:23 +0200

    rtzd

---
+++ JS/idopont.js
@@ -0,0 +1,54 @@
+/* Adatok */
+
+let veznev = document.getElementById("veznev")
+let kernev = document.getElementById("kernev")
+let telefon = document.getElementById("telefon")
+let email = document.getElementById("emaill")
+
+/* Ok */
+
+let laborcs = document.getElementById("laborcs")
+let szolgalt = document.getElementById("szolgalt")
+let mind2 = document.getElementById("mind2")
+
+/* Épületek */
+
+let aEpulet = document.getElementById("aEpulet")
+let bEpulet = document.getElementById("bEpulet")
+let terkepz = document.getElementById("terkepz") 
+
+/* Laborcsomag */
+
+let genetika = document.getElementById("genetika")
+let alapegesz = document.getElementById("alapegesz")
+let diagnosztika = document.getElementById("diagnosztika")
+let sportolo = document.getElementById("sportolo")
+let elelmiszer = document.getElementById("elelmiszer")
+let kornyezet2 = document.getElementById("kornyezet2")
+
+let genetikaGomb = document.getElementById("genetikaGomb")
+let alapegeszGomb = document.getElementById("alapegeszGomb")
+let diagnosztikaGomb = document.getElementById("diagnosztikaGomb")
+let sportoloGomb = document.getElementById("sportoloGomb")
+let elelmiszerGomb = document.getElementById("elelmiszerGomb")
+let kornyezet2Gomb = document.getElementById("kornyezet2Gomb")
+
+/* Szolgáltatás */
+
+let vervizs = document.getElementById("vervizs")
+let mikroB = document.getElementById("mikroB")
+let genetikaiV = document.getElementById("genetikaiV")
+let kornyezet = document.getElementById("kornyezet")
+let elelmiszer2 = document.getElementById("elelmiszer2")
+let gyogyszer = document.getElementById("gyogyszer")
+let hormon = document.getElementById("hormon")
+let immunologia = document.getElementById("immunologia")
+
+let vervizsGomb = document.getElementById("vervizsGomb")
+let mikroBGomb = document.getElementById("mikroBgomb")
+let genetikaiVGomb = document.getElementById("genetikaiVgomb")
+let kornyezetGomb = document.getElementById("kornyezetGomb")
+let elelmiszer2Gomb = document.getElementById("elelmiszer2Gomb")
+let gyogyszerGomb = document.getElementById("gyogyszerGomb")
+let hormonGomb = document.getElementById("hormonGomb")
+let immunologiaGomb = document.getElementById("immunologiaGomb")
